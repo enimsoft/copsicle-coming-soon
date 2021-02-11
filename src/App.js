d1afe17e7fae70dfc50fca01dce3a0e1fd6d5b47
aakash0017 <akashbharat.bhatnagar@gmail.com>
2021-02-11 12:44:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ import ComingSoon from "react-coming-soon";
 function App() {
   return (
     <div className="App">
-     <ComingSoon
-       image="https://react-coming-soon.maksv.me/default-image.jpeg"
-       bgColor="#fff"
-       textColor="#000"
-       illustration="development"
-     />
+     
     </div>
   );
 }
